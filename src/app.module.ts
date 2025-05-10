c7d3e74e8ab3571de256da83e70d25f640621eab
HiLsAger <47248609+HiLsAger@users.noreply.github.com>
2025-05-10 14:58:55 +0300

    добавил настройки среды

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { ModuleModules } from "./module/Modules.module";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [ModuleModules],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ".env",
+      isGlobal: true,
+    }),
+    ModuleModules,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/module/datebase/datebase.module.ts
+++ src/module/datebase/datebase.module.ts
@@ -1,16 +1,25 @@
 import { Module } from "@nestjs/common";
 import { SequelizeModule } from "@nestjs/sequelize";
 import { models } from "./models";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { config } from "rxjs";
 
 @Module({
   imports: [
-    SequelizeModule.forRoot({
-      dialect: "mysql",
-      database: "nest-crm",
-      host: "localhost",
-      models: models,
-      username: "hils",
-      password: "0497",
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) =>  ({
+        dialect: "mysql",
+        host: config.get("DB_HOST"),
+        port: config.get<number>("DB_PORT"),
+        database: config.get("DB_NAME"),
+        username: config.get("DB_USER"),
+        password: config.get("DB_PASSWORD"),
+        models: models,
+        autoLoadModels: true,
+        synchronize: true,
+      }),
     }),
   ],
 })
